da420c83a7da551ff45f83a7623e3bb73c843638
Yoel <yoel.coriat@uip.edu.pa>
2026-03-12 08:42:37 -0500

    Simplificar codigo y añadir comentarios

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-# modulo de contabilidad es placeholder, lo pueden cambiar cuando quieran
-run = True
+# Modulo de contabilidad es placeholder, lo pueden cambiar a algo mas comodo
 
-while run:
+# Yoel Coriat va a realizar el archivo de matematicas
+# Maryane puede realizar el de contacuentos
+# Y Jose puede realizar el ultimo de contabilidad, aunque puedes cambiar el modulo a que sea lo que quieras
+
+
+while True:
     modulo = input("Cual modulo desea utilizar?\n\n"
                    "1 - Modulo de matematicas\n"
                    "2 - Modulo de cuentacuentos\n"
                    "3 - Modulo de contabilidad\n")
 
+    # Debajo de cada condicional pueden ejecutar su modulo
+    # Es decir, importan el archivo .py arriba al inicio del archivo y ejecutan una funcion como modulo1.run()
     if modulo == "1":
-        run = False
-        # funcion para ejecutar el modulo 1 importado
+        break
 
     elif modulo == "2":
-        run = False
-        # funcion para ejecutar el modulo 2 importado
+        break
 
     elif modulo == "3":
-        run = False
-        # funcion para ejecutar el modulo 3 importado
+        break
 
     else:
         print("El modulo ingresado no existe. Por favor ingresar un modulo del 1-3: ")
